8c504c1b20af6b61216c59f2a4889b6d3961fe02
Philipp Janda <siffiejoe@gmx.net>
2013-09-04 17:16:52 +0200

    implemented enhanced load* functions

--- compat52.lua
+++ compat52.lua
@@ -7,8 +7,91 @@ if _VERSION == "Lua 5.1" then
    
    local unavailable = function() error("compat52: Do not use this function. It is incompatible with Lua 5.2") end
    
+   local _setfenv = setfenv
    setfenv = unavailable
    getfenv = unavailable
+
+   local function check_mode(mode, prefix)
+      local has = { text = false, binary = false }
+      for i = 1,#mode do
+         local c = mode:sub(i, i)
+         if c == "t" then has.text = true end
+         if c == "b" then has.binary = true end
+      end
+      local t = prefix:sub(1, 1) == "\27" and "binary" or "text"
+      if not has[t] then
+         return "attempt to load a "..t.." chunk (mode is '"..mode.."')"
+      end
+   end
+
+   local _load, _loadstring = load, loadstring
+   load = function(ld, source, mode, env)
+      mode = mode or "bt"
+      local chunk, msg
+      if type( ld ) == "string" then
+         if mode ~= "bt" then
+            local merr = check_mode(mode, ld)
+            if merr then return nil, merr end
+         end
+         chunk, msg = _loadstring(ld, source)
+      else
+         local ld_type = type(ld)
+         if ld_type ~= "function" then
+            error("bad argument #1 to 'load' (function expected, got "..ld_type..")")
+         end
+         if mode ~= "bt" then
+            local checked, merr = false, nil
+            local function checked_ld()
+               if checked then
+                  return ld()
+               else
+                  checked = true
+                  local v = ld()
+                  merr = check_mode(mode, v or "")
+                  if merr then return nil end
+                  return v
+               end
+            end
+            chunk, msg = _load(checked_ld, source)
+            if merr then return nil, merr end
+         else
+            chunk, msg = _load(ld, source)
+         end
+      end
+      if not chunk then
+         return chunk, msg
+      end
+      if env ~= nil then
+         _setfenv(chunk, env)
+      end
+      return chunk
+   end
+
+   loadstring = load
+
+   local _loadfile = loadfile
+   loadfile = function(file, mode, env)
+      mode = mode or "bt"
+      if mode ~= "bt" then
+         local f = io.open(file, "rb")
+         if f then
+            local prefix = f:read(1)
+            f:close()
+            if prefix then
+               local merr = check_mode(mode, prefix)
+               if merr then return nil, merr end
+            end
+         end
+      end
+      local chunk, msg = _loadfile(file)
+      if not chunk then
+         return chunk, msg
+      end
+      if env ~= nil then
+         _setfenv(chunk, env)
+      end
+      return chunk
+   end
    
    local os_execute = os.execute
    os.execute = function(cmd)
