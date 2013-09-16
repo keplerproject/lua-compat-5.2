edbbb2ac3d040edb26c2e348c0e7660da19ae22d
Philipp Janda <siffiejoe@gmx.net>
2013-09-16 17:41:16 +0200

    implemented package.searchpath

--- compat52.lua
+++ compat52.lua
@@ -212,6 +212,23 @@ if _VERSION == "Lua 5.1" then
       end
    end
 
+   package.searchpath = function(name, path, sep, rep)
+      sep = (sep or "."):gsub("(%p)", "%%%1")
+      rep = (rep or package.config:sub(1, 1)):gsub("(%%)", "%%%1")
+      local pname = name:gsub(sep, rep):gsub("(%%)", "%%%1")
+      local msg = {}
+      for subpath in path:gmatch("[^;]+") do
+         local fpath = subpath:gsub("%?", pname)
+         local f = io.open(fpath, "r")
+         if f then
+            f:close()
+            return fpath
+         end
+         msg[#msg+1] = "\n\tno file '" .. fpath .. "'"
+      end
+      return nil, table.concat(msg)
+   end
+
    local p_index = { searchers = package.loaders }
    setmetatable(package, {
       __index = p_index,
